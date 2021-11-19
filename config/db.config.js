44115838242f65e6e0085ea979dea56c77595c6b
Tomras2 <tomorune@gmail.com>
2021-11-19 11:36:59 +0200

    changed

--- config/db.config.js
+++ config/db.config.js
@@ -2,23 +2,23 @@
 
 const mysql = require('mysql');
 
-// const dbConn = mysql.createConnection(
-//     {
-//         host : 'localhost',
-//         user : 'root',
-//         password : '',
-//         database : 'sportsdb'
-//     }
-// );
-
 const dbConn = mysql.createConnection(
     {
-        host : 'us-cdbr-east-04.cleardb.com',
-        user : 'b3ef48ffac1cd6',
-        password : '48e4275c',
-        database : 'heroku_66f4623ceb2cf7f'
+        host : 'localhost',
+        user : 'root',
+        password : '',
+        database : 'sportsdb'
     }
 );
+
+// const dbConn = mysql.createConnection(
+//     {
+//         host : 'us-cdbr-east-04.cleardb.com',
+//         user : 'b3ef48ffac1cd6',
+//         password : '48e4275c',
+//         database : 'heroku_66f4623ceb2cf7f'
+//     }
+// );
 
 dbConn.connect(function(err) {
     if (err) throw err;
