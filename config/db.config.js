4b2892225625b9ddb295f3206de391fb136c98d8
Tomras2 <tomorune@gmail.com>
2021-12-08 02:00:00 +0200

    upd

--- config/db.config.js
+++ config/db.config.js
@@ -12,40 +12,86 @@ const mysql = require("mysql");
 //     }
 // );
 
-var db_config = {
+var connectionState = false;
+var connection = mysql.createConnection({
   host: "us-cdbr-east-04.cleardb.com",
   user: "b3ef48ffac1cd6",
-  password: "48e4275c",
+  password:"48e4275c",
   database: "heroku_66f4623ceb2cf7f",
-};
-var connection;
+  insecureAuth: true
+});
+connection.on('close', function (err) {
+  logger.error('mysqldb conn close');
+  connectionState = false;
+});
+connection.on('error', function (err) {
+  logger.error('mysqldb error: ' + err);
+  connectionState = false;
+});
 
-function handleDisconnect() {
-  connection = mysql.createConnection(db_config); // Recreate the connection, since
-  // the old one cannot be reused.
+function attemptConnection(connection) {
+  if(!connectionState){
+    connection = mysql.createConnection(connection.config);
+    connection.connect(function (err) {
+      // connected! (unless `err` is set)
+      if (err) {
+        logger.error('mysql db unable to connect: ' + err);
+        connectionState = false;
+      } else {
+        logger.info('mysql connect!');
 
-  connection.connect(function (err) {
-    // The server is either down
-    if (err) {
-      // or restarting (takes a while sometimes).
-      console.log("error when connecting to db:", err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    } // to avoid a hot loop, and to allow our node script to
-  }); // process asynchronous requests in the meantime.
-  // If you're also serving http, display a 503 error.
-  connection.on("error", function (err) {
-    console.log("db error", err);
-    if (err.code === "PROTOCOL_CONNECTION_LOST") {
-      // Connection to the MySQL server is usually
-      handleDisconnect(); // lost due to either server restart, or a
-    } else {
-      // connnection idle timeout (the wait_timeout
-      throw err; // server variable configures this)
-    }
-  });
+        connectionState = true;
+      }
+    });
+    connection.on('close', function (err) {
+      logger.error('mysqldb conn close');
+      connectionState = false;
+    });
+    connection.on('error', function (err) {
+      logger.error('mysqldb error: ' + err);
+
+      if (!err.fatal) {
+        //throw err;
+      }
+      if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
+        //throw err;
+      } else {
+        connectionState = false;
+      }
+
+    });
+  }
 }
+attemptConnection(connection);
 
-handleDisconnect();
+var dbConnChecker = setInterval(function(){
+  if(!connectionState){
+    logger.info('not connected, attempting reconnect');
+    attemptConnection(connection);
+  }
+}, 2000);
+
+// Mysql query wrapper. Gives us timeout and db conn refreshal! 
+var queryTimeout = 2000;
+var query = function(sql,params,callback){
+  if(connectionState) {
+    // 1. Set timeout
+    var timedOut = false;
+    var timeout = setTimeout(function () {
+      timedOut = true;
+      callback('MySQL timeout', null);
+    }, queryTimeout);
+
+    // 2. Make query
+    connection.query(sql, params, function (err, rows) {
+      clearTimeout(timeout);
+      if(!timedOut) callback(err,rows);
+    });
+  } else {
+    // 3. Fail if no mysql conn (obviously)
+    callback('MySQL not connected', null);
+  }
+}
 
 // dbConn.connect(function(err) {
 //     if (err) throw err;
