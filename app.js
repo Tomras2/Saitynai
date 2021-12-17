00c848ddf42ec4186886288574554f0cebb6b3d1
Tomras2 <tomorune@gmail.com>
2021-12-17 05:09:21 +0200

    updates

--- app.js
+++ app.js
@@ -9,10 +9,8 @@ const cors = require("cors");
 //   };
 
 const corsOptions = {
-    origin:'*', 
+    origin:"https://sportshopweb.herokuapp.com/", 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE, OPTIONS',
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200,
  }
   
 app.use(cors(corsOptions));
@@ -44,15 +42,14 @@ app.use(bodyParser.json({
 //   });
 
 
-// app.use(function(req, res, next) {
-//     res.header(
-//         "Access-Control-Allow-Headers",
-//         "x-access-token, Origin, Content-Type, Accept"
-//     );
-//     next();
-// });
+app.use(function(req, res, next) {
+    res.header(
+        "Access-Control-Allow-Headers",
+        "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+});
 
-app.use(cors());
 app.use((err, req, res, next) => {
     // This check makes sure this is a JSON parsing issue, but it might be
     // coming from any middleware, not just body-parser:
