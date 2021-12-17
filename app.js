c5d3e2afefecb9a5e687c764aa818d0741765103
Tomras2 <tomorune@gmail.com>
2021-12-17 03:18:57 +0200

    upd

--- app.js
+++ app.js
@@ -4,11 +4,20 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require("cors");
 
-var corsOptions = {
-    origin: "http://localhost:8081"
-  };
+// var corsOptions = {
+//     origin: "http://localhost:8081"
+//   };
+
+  const corsOptions = {
+    origin:'*', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
   
 app.use(cors(corsOptions));
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
