cebcfc85957cd4a8a2ef6254eac0499f12afdc7d
Tomras2 <tomorune@gmail.com>
2021-12-17 04:20:22 +0200

    updates

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(express.methodOverride());
 // 
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, POST, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
       
     // intercept OPTIONS method
