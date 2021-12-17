b7db280b603301110876d48fe76057226d587566
Tomras2 <tomorune@gmail.com>
2021-12-17 03:45:46 +0200

    updates

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const cors = require("cors");
 //   };
 
   const corsOptions = {
-    origin:'https://sportshopweb.herokuapp.com', 
+    origin:'https://sportshopweb.herokuapp.com',
+    credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
  }
   
 app.use(cors(corsOptions));
