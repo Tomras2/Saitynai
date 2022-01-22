4fea9efaca9b913b79ee1959c8b89c1241405b41
Tomras2 <tomorune@gmail.com>
2022-01-22 18:57:59 +0200

    tryfix

--- app.js
+++ app.js
@@ -36,7 +36,8 @@ app.use(bodyParser.json({
 app.use(function(req, res, next) {
     res.header(
         "Access-Control-Allow-Headers",
-        "x-access-token, Origin, Content-Type, Accept"
+        "x-access-token, Origin, Content-Type, Accept",
+        'Access-Control-Allow-Origin: *'
     );
     next();
 });
