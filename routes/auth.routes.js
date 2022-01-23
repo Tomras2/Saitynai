2ce64e1c159790e0be180505532efb0de3f230a1
Tomras2 <tomorune@gmail.com>
2022-01-23 15:26:48 +0200

    tryfix

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,13 +6,13 @@ const router = express.Router()
 
 
   
-    // app.use(function(req, res, next) {
-    //     res.header(
-    //         "Access-Control-Allow-Headers",
-    //         "x-access-token, Origin, Content-Type, Accept"
-    //     );
-    //     next();
-    // });
+    app.use(function(req, res, next) {
+        res.header(
+            "Access-Control-Allow-Headers",
+            "x-access-token, Origin, Content-Type, Accept"
+        );
+        next();
+    });
 
     router.post(
         "/signup",
